9b16ae356414d9d048686778c80b19adfdfb9c0e
Markus Budeus <markus.budeus@web.de>
2023-01-31 11:18:36 +0100

    Made a sadly very unsuccessful training of the LSTM predictor

---
+++ ai-lecture-project/datasets/graph_dataset.py
@@ -0,0 +1,23 @@
+import pickle
+from typing import List
+
+from torch.utils.data import Dataset
+
+from graph import Graph
+
+
+class GraphDataset(Dataset):
+    """
+    This dataset provides lists of parts as inputs and full graphs as labels.
+    """
+
+    def __init__(self):
+        with open('../data/graphs.dat', 'rb') as file:
+            self.graphs: List[Graph] = pickle.load(file)
+
+    def __len__(self):
+        return len(self.graphs)
+
+    def __getitem__(self, idx):
+        graph = self.graphs[idx]
+        return graph.get_parts(), graph
